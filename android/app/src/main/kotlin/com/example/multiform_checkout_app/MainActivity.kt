41b10e7493552eeaa474e48b1dc20032110bd125
KimtaiKogoKevin <49808720+KimtaiKogoKevin@users.noreply.github.com>
2022-08-10 18:58:57 +0300

    logic and ui

---
+++ android/app/src/main/kotlin/com/example/multiform_checkout_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multiform_checkout_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider<UserRepository>(
+      create: (_) => UserRepository(),
+      child: MaterialApp(
+        title: 'Flutter Multi-Page Form',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+        ),
+        home: const CheckoutPage(),
+      ),
+    );
+  }
+}
